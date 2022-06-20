2555c3d1208987a7d0b7d9b4f609421289b6f565
JackDev42 <jacek.romasevski@gmail.com>
2022-06-20 16:41:59 +0300

    updated plugin version

--- spell-woocommerce/spell-woocommerce.php
+++ spell-woocommerce/spell-woocommerce.php
@@ -4,7 +4,7 @@
  * Plugin Name: Klix E-commerce Gateway
  * Plugin URI:
  * Description: Klix E-commerce Gateway
- * Version: 1.1.2e5
+ * Version: 1.1.2e6
  * Author: Klix
  * Author URI:
  * Developer: Klix
